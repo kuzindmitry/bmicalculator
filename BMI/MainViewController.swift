c19fd790b3e0f6810c15a31b8c3327cb1e26e35d
Kuzin Dmitry <kuzindo@gmail.com>
2019-08-30 19:55:55 +0300

    Add charts

--- BMI/MainViewController.swift
+++ BMI/MainViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var chartView: ChartView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        chartView.isCurved = true
+        chartView.dataEntries = generateRandomData()
+        chartView.backgroundColor = .clear
+    }
+    
+    func generateRandomData() -> [PointEntry] {
+        var result: [PointEntry] = []
+        for i in 0..<100 {
+            let value = Int(arc4random() % 500)
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMM"
+            var date = Date()
+            date.addTimeInterval(TimeInterval(24*60*60*i))
+            
+            result.append(PointEntry(value: value, label: formatter.string(from: date)))
+        }
+        return result
     }
 
 
